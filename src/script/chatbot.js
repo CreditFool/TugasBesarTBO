9148fd43c8f73f9ac5929864ec2c6016550f50a6
creditFool <bichlasulbulqiah@gmail.com>
2020-12-07 16:29:50 +0700

    memulai pembuatan fitur pencarian nama ibu kota

--- src/script/chatbot.js
+++ src/script/chatbot.js
@@ -4,6 +4,64 @@
 
 //------------------------------------------------
 
+let state = 'awal';
+
+
+//async function fetchData() {
+//        try {
+//            const response = await fetch(`https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json`);
+//            const data = await response.json();
+//            return data;
+//        } catch (error) {
+//            console.error(error);
+//        }
+//    }
+
+let kota;
+
+fetch('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json')
+  .then(res => res.json())
+  .then(data => kota = data)
+
+function getKota(negara) {
+    
+//    const list = await fetchData();
+    const list = kota;
+    let hasil;
+    
+    for (let i=0; i<list.length; i++) {
+        if (list[i].country == negara) {
+            hasil = list[i].city;
+            break;
+        }
+    }
+    
+    return hasil;
+//    console.log(hasil);
+//    return hasil;
+}
+
+//function getIbuKota(negara = "Indonesia") {
+//    
+//    let hasil;
+//    
+//    fetch('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json')
+//    // Handle success
+//        .then(response => response.json())  // convert to json
+//        .then(json => {
+//            if (json.error) {
+//                console.log('error');
+//            }
+//            else {
+////                addChat('dia', ambil(json, negara));
+//                let yay = ambil(json, negara);
+//                console.log(yay);
+//            }
+//        })
+//        .catch(err => console.log('Request Failed', err)); // Catch errors
+//    
+//    return hasil;
+//}
 
 function resetChatValue() {
     document.getElementById("chat-input").value = "";
@@ -17,48 +75,73 @@ function getRandomNumber(num) {
     return Math.floor((Math.random() * num));
 }
 
+
+
 function getResponse(chatText) {
     const resp = {
-        "keyword": [
-            "keyword yang tersedia saat ini:<br>- kelompok<br>- judul<br>- test<br>- halo"
-        ],
-        "kelompok": [
+        "awal" : {
+            "keyword": [ 
+//            "bababoy"
+                getKota("Indonesia")
+            ],
+            "kelompok": [
             `
             Kelompok 1 <br /><br />
             Ashril Argadeda Mahendra (190535646066)<br />
             Ichlalsul Bulqiah (190535646047)<br />
             Mawaddah Awaliyah (190535646089)<br />
             `
-        ],
-        "judul": [
-            "Implementasi finite state automata pada pembuatan chatbot"
-        ],
-        "test": [
-            "ini adalah contoh balasan pertama (balasan pada key ini acak)",
-            "ini adalah contoh balasan kedua (balasan pada key ini acak)",
-            "ini adalah contoh balasan ketiga (balasan pada key ini acak)"
-        ],
-        "halo": [
-            "hai",
-            "halo juga",
-            "yahoo"
-        ],
-        "else": [
-            "silahkan ketik 'keyword' kemudian tekan enter"         
-        ]
+            ],
+            "judul": [
+                "Implementasi finite state automata pada pembuatan chatbot"
+            ],
+            "test": [
+                "ini adalah contoh balasan pertama (balasan pada key ini acak)",
+                "ini adalah contoh balasan kedua (balasan pada key ini acak)",
+                "ini adalah contoh balasan ketiga (balasan pada key ini acak)"
+            ],
+            "halo": [
+                "hai",
+                "halo juga",
+                "yahoo"
+            ],
+            "else": [
+                "silahkan ketik 'keyword' kemudian tekan enter"         
+            ]
+        },
+        
+        "cari_negara" : {
+            "petunuk" : [
+                
+            ],
+            
+            "selesai" : [
+                
+            ],
+            
+            "else" : [
+                getKota(chatText)
+            ]
+        }
+        
     }
     
-    chatText = chatText.toLowerCase();
-    if (chatText in resp) {
-        const rand = getRandomNumber(resp[chatText].length);
-        console.log(rand);
-        return resp[chatText][rand];
-    }
-    else {
-        const rand = getRandomNumber(resp["else"].length);
-        console.log(rand);
-        return resp["else"][rand];
-    }
+//    console.log(resp[state][chatText][0]);
+    console.log(resp["cari_negara"]["else"][0]);
+    addChat("dia", resp["cari_negara"]["else"][0]);
+    
+    
+//    chatText = chatText.toLowerCase();
+//    if (chatText in resp) {
+//        const rand = getRandomNumber(resp[chatText].length);
+//        console.log(rand);
+//        return resp[chatText][rand];
+//    }
+//    else {
+//        const rand = getRandomNumber(resp["else"].length);
+//        console.log(rand);
+//        return resp["else"][rand];
+//    }
 }
 
 function addChat(sender, chatText) {
@@ -80,7 +163,8 @@ const sendButton = document.getElementById("send-button");
 sendButton.addEventListener("click", function(event){
     if (!(getChatValue() == false)) {
         addChat("aku", getChatValue());
-        addChat("dia", getResponse(getChatValue()));
+         addChat("dia", getResponse(getChatValue()));
+        
         resetChatValue();
         updateScroll();
         
